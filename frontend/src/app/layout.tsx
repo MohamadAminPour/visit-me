746ea73032576ba93222a765c5c0d7679e1c970a
mohammad amin pour <ytornado37@gmail.com>
2025-10-11 19:19:18 +0330

    start project

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
