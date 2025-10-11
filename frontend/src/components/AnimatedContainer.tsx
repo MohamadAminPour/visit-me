11ebd212270de4e9ae2c0e2d0c77bedd9793db51
mohammad amin pour <ytornado37@gmail.com>
2025-10-11 19:21:47 +0330

    create intro page

---
+++ frontend/src/components/AnimatedContainer.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { motion } from "framer-motion";
+
+export default function AnimatedContainer({ children }: any) {
+  return (
+    <motion.div
+      initial={{ opacity: 0,  }}
+      animate={{ opacity: 1, }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+      className="flex items-center justify-center flex-col h-screen px-[1rem] md:mx-0"
+    >
+      {children}
+    </motion.div>
+  );
+}
