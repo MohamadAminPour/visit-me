9f803af679c8883b2b8e644f2b23afe55ebdc0db
mohammad amin pour <ytornado37@gmail.com>
2025-10-13 14:50:35 +0330

    add CrispChat to project

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import CrispChatWithStatus from "@/components/CrispChat";
 
 export default function RootLayout({
   children,
@@ -8,7 +9,10 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">
       <body>
+       <>
         {children}
+        <CrispChatWithStatus websiteId="c1cc9cc8-ac2d-4850-a0b7-d447d8605da9"/>
+       </>
       </body>
     </html>
   );
