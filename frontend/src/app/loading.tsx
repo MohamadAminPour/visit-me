1914adcf6934bc67a1af7f01da8c8d309de9db17
mohammad amin pour <ytornado37@gmail.com>
2025-10-19 18:27:03 +0330

    add loader to project

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex items-center justify-center h-screen bg-red-300'>
+      <p>در حال بارگذاری...</p>
+    </div>
+  )
+}
---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <div className='flex items-center justify-center bg-white h-[70vh] '>
+      <div className='loader'></div>
+    </div>
+  )
+
+  
+}
