ce375089544c62f5bf89559540f23a963ef25e30
mohammad amin pour <ytornado37@gmail.com>
2025-10-21 19:55:47 +0330

    edit loading component

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function loading() {
   return (
-    <div className='flex items-center justify-center h-screen bg-red-300'>
+    <div className='flex items-center justify-center h-screen'>
       <p>در حال بارگذاری...</p>
     </div>
   )
